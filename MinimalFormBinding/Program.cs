441c027ae46e75b175b8a601347d027236005d3c
Safia Abdalla <safia@safia.rocks>
2023-11-13 12:49:18 -0800

    Update form style

--- MinimalFormBinding/Program.cs
+++ MinimalFormBinding/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +12,55 @@ app.UseAntiforgery();
 app.MapGet("/", (HttpContext context, IAntiforgery antiforgery) =>
 {
     var token = antiforgery.GetAndStoreTokens(context);
-    var html = $"""
+    var html = $$"""
         <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 20px;
+                    }
+
+                    form {
+                        max-width: 400px;
+                        margin: 0 auto;
+                    }
+
+                    label {
+                        display: block;
+                        margin-top: 10px;
+                    }
+
+                    input, select {
+                        width: 100%;
+                        padding: 8px;
+                        margin-top: 5px;
+                        margin-bottom: 10px;
+                        box-sizing: border-box;
+                    }
+
+                    button {
+                        background-color: #4CAF50;
+                        color: white;
+                        padding: 10px 15px;
+                        border: none;
+                        border-radius: 4px;
+                        cursor: pointer;
+                    }
+
+                    button:hover {
+                        background-color: #45a049;
+                    }
+                </style>
+            </head>
             <body>
                 <form action="/todos" method="POST" enctype="multipart/form-data">
-                    <input name="{token.FormFieldName}" type="hidden" value="{token.RequestToken}" />
+                    <input name="{{token.FormFieldName}}" type="hidden" value="{{token.RequestToken}}" />
+                    <label for="name">Todo Name:</label>
                     <input type="text" name="name" />
+                    <label for="dueDate">Due Date:</label>
                     <input type="date" name="dueDate" />
+                    <label for="isCompleted">Done:</label>
                     <input type="checkbox" name="isCompleted" />
                     <input type="submit" />
                 </form>
