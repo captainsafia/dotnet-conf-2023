e300c8327c3ab618af9195737785ba9094c2bafc
Safia Abdalla <safia@safia.rocks>
2023-11-13 14:52:48 -0800

    Remove analyzer demo

--- NewAnalyzers/Program.cs
+++ NewAnalyzers/Program.cs
@@ -4,20 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ASP0025
 builder.Services.AddAuthentication();
-builder.Services.AddAuthorization(configure: options => 
-    options.AddPolicy("required-role", policy =>
-        policy.RequireRole("office-admin")));
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("required-role", policy =>
+        policy.RequireRole("office-admin"));
 
 var app = builder.Build();
 
-// ASP0016
-app.MapGet("/asp0016", context => Task.FromResult(context.Request.Query["name"]);
-
 // ASP0020
-app.MapGet("/asp0020/{todo}", (Todo todo) => todo);
+app.MapPost("/asp0020", (Todo todo) => todo);
 
 // ASP0024
-app.MapPost("/asp0024", ([FromBody] Todo todo, [FromBody] ITodoService todoService)
+app.MapPost("/asp0024", ([FromBody] Todo todo, ITodoService todoService)
     => todoService.AddTodo(todo));
 
 app.Run();
