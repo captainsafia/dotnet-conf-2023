e487ceb6af455fda4af62ae3c282f33951ba3cf6
Safia Abdalla <safia@safia.rocks>
2023-11-13 12:39:25 -0800

    Update new analyzers sample

--- NewAnalyzers/Program.cs
+++ NewAnalyzers/Program.cs
@@ -2,10 +2,17 @@ using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// ASP0025
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization(configure: options => 
+    options.AddPolicy("required-role", policy =>
+        policy.RequireRole("office-admin")));
+
 var app = builder.Build();
 
 // ASP0016
-app.MapGet("/asp0016", (HttpContext context) => Task.FromResult(context.Request.Query["name"]));
+app.MapGet("/asp0016", context => Task.FromResult(context.Request.Query["name"]));
 
 // ASP0020
 app.MapGet("/asp0020/{todo}", (Todo todo) => todo);
@@ -13,20 +20,6 @@ app.MapGet("/asp0020/{todo}", (Todo todo) => todo);
 // ASP0024
 app.MapPost("/asp0024", ([FromBody] Todo todo, [FromBody] ITodoService todoService)
     => todoService.AddTodo(todo));
-
-// ASP0015
-app.MapGet("/asp0015", (HttpRequest request) =>
-{
-    if (request.Headers["Content-Type"] != "application/json")
-    {
-        return null;
-    }
-
-    var versionNumber = request.Headers["X-Api-Version"];
-    var branchName = request.Headers["X-Branch-Name"];
-
-    return new { versionNumber, branchName };
-});
 
 app.Run();
 
