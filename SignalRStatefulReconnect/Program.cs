d96793aace3c04b174e4ff788330cbaee82f2a0c
Stephen Halter <halter73@gmail.com>
2023-11-14 15:05:06 -0800

    Add ability to reset WebSocket from command line

--- SignalRStatefulReconnect/Program.cs
+++ SignalRStatefulReconnect/Program.cs
@@ -18,4 +18,52 @@ app.MapHub<ChatHub>("/chat", signalRConnectionOptions =>
     signalRConnectionOptions.AllowStatefulReconnects = true;
 });
 
-app.Run();
+var httpContextLock = new object();
+HttpContext? lastWebSocketHttpContext = null;
+
+app.UseWebSockets();
+app.Use(next =>
+{
+    return async httpContext =>
+    {
+        if (httpContext.WebSockets.IsWebSocketRequest)
+        {
+            Console.WriteLine($"Enter 'r' to reset {httpContext.Request.Query["name"]}'s WebSocket.");
+            lastWebSocketHttpContext = httpContext;
+        }
+
+        await next(httpContext);
+
+        lock (httpContextLock)
+        {
+            if (lastWebSocketHttpContext == httpContext)
+            {
+                lastWebSocketHttpContext = null;
+            }
+        }
+    };
+});
+await app.StartAsync();
+var lifetimeTask = app.WaitForShutdownAsync();
+
+Console.WriteLine($"Enter 'q' to stop the server.");
+
+while (!lifetimeTask.IsCompleted)
+{
+    var key = Console.Read();
+    if (key == 'r')
+    {
+        lock (httpContextLock)
+        {
+            if (lastWebSocketHttpContext is not null)
+            {
+                Console.WriteLine("Reset WebSocket.");
+                lastWebSocketHttpContext?.Abort();
+            }
+        }
+    }
+    else if (key == 'q')
+    {
+        await app.StopAsync();
+    }
+}
