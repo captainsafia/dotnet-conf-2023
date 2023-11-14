2ef7a43680b225b5ac6f3560501ac2ae3f3631aa
Safia Abdalla <safia@safia.rocks>
2023-11-14 14:10:11 -0800

    Reset analyzer demo state

--- NewAnalyzers/Program.cs
+++ NewAnalyzers/Program.cs
@@ -4,17 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ASP0025
 builder.Services.AddAuthentication();
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("required-role", policy =>
-        policy.RequireRole("office-admin"));
+builder.Services.AddAuthorization(configure: options => 
+     options.AddPolicy("required-role", policy =>
+         policy.RequireRole("office-admin")));
 
 var app = builder.Build();
 
 // ASP0020
-app.MapPost("/asp0020", (Todo todo) => todo);
+app.MapPost("/asp0020/{todo}", (Todo todo) => todo);
 
 // ASP0024
-app.MapPost("/asp0024", ([FromBody] Todo todo, ITodoService todoService)
+app.MapPost("/asp0024", ([FromBody] Todo todo, [FromBody] ITodoService todoService)
     => todoService.AddTodo(todo));
 
 app.Run();
